ef2250b1cb6e14ead3d9317e26927fa0caa4b4ca
rayanenth <rayanenth09@gmail.com>
2024-08-19 14:29:11 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+  background("purple");
+  }
+  
+  function draw() {
+    stroke("black");
+    fill("pink");
+     
+    if(mouseIsPressed){
+    rect(mouseX,mouseY,20,35);
+    }
+  
+  }
